33bd99d2299d1b0d8c7517ab367a84a61a606762
anfeng-oliver <2314010089@qq.com>
2019-10-20 22:15:41 -0700

    exam2

---
+++ feng/exam2/code/3-2.c
@@ -0,0 +1,24 @@
+#include "ourhdr.h"
+
+char buf1[] = "abcdefghij";
+char buf2[] = "ABCDEFGHIJ";
+
+int main(void){
+	int fd;
+
+	if ( (fd = creat("file.hole",FILE_MODE)) < 0)
+		perror("creat error");
+
+	if (write(fd,buf1,10)!= 10)
+		perror("buf1 write error");
+	/*offset now = 10*/
+
+	if (lseek(fd,40,SEEK_SET) == -1)
+		perror("lseek error");
+	/*offset now = 40*/
+
+	if (write(fd,buf2,10)!=10)
+		perror("buf2 write error");
+
+	exit(0);
+}
---
+++ feng/exam2/code/ourhdr.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+#define DIR_MODE (FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
