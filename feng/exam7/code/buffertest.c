0ce5b6522fee889fd537d61fcd6153bc971004d5
anfeng-oliver <2314010089@qq.com>
2019-10-22 01:20:01 -0700

    add exam7

---
+++ feng/exam7/code/buffertest.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 512
+int main(int argc,char *argv[])
+{
+	char buf[SIZE];
+	if(setvbuf(stdin, buf, _IONBF, SIZE)!=0)
+	{
+		perror("No buffer stdin setting failed!\n");
+		return 1;
+	}	
+	printf("No buffer stdin setting succeed!\n");
+	printf("kind of the stdin :\n");
+	if(stdin->_flags & _IO_UNBUFFERED)
+	{
+		printf("No buffer!\n");
+	}
+	else if(stdin->_flags & _IO_LINE_BUF)
+	{
+		printf("Line buffer!\n");
+	}
+	else
+	{
+		printf("Fully buffer!\n");
+	}
+	printf("Size Of Buffer Area is %ld\n",stdin->_IO_buf_end - stdin->_IO_buf_base);
+	printf("File desc symbol is %d\n",fileno(stdin));
+	if(setvbuf(stdin,buf,_IOFBF,SIZE)!=0)
+	{
+		printf("Fully buffer stdin setting failed!\n");
+		return 2;
+	}
+	printf("Modify stdin kind succeed!\n");
+	printf("Kind of the stdin is :\n");
+	if(stdin->_flags &_IO_UNBUFFERED)
+	{
+		printf("No buffer!\n");
+	}else if(stdin->_flags &_IO_LINE_BUF)
+	{
+		printf("Line buffer!\n");
+	}
+	else
+	{
+		printf("Fully buffer!\n");
+	}
+	printf("Size Of Buffer Area is %ld\n",stdin->_IO_buf_end - stdin->_IO_buf_base);
+	printf("File desc symbol is %d\n",fileno(stdin));
+	return 0;
+}
+
